430eed96339c2254a2226b0edf669b4f08f8761c
dylanvisher <dylan.visher@yale.edu>
2013-07-26 16:14:31 -0400

    save and load object prototypes running, need to add in stored long and short axes for the Stored PSObject

--- src/server.cpp
+++ src/server.cpp
@@ -3,6 +3,8 @@
 // Date: 5/18/13
 // About: ROS node for tracking objects in the PhaseSpace System
 
+#include <sys/stat.h>
+#include <sys/types.h>
 // Include the ROS system
 #include <ros/ros.h>
 // rviz integration
@@ -29,13 +31,8 @@
 #include "core_object_server/save_object.h"
 #include "core_object_server/load_object.h"
 #include "core_object_server/add_manual.h"
-// need to add add_manual_object
-//             delete_manual_object
-//             functionality for save and load manual object
-//             store and entire environment
-//             load an entire environment
-// maybe each object should emit vector of object id's it is colliding with
-
+#include "core_object_server/save_env.h"
+#include "core_object_server/load_env.h"
 
 // Set up ROS messages
 #include "std_msgs/String.h"
@@ -224,7 +221,7 @@ vector<Point> get_points(int time, bool glove = false) {
   return points;
 }
 bool store_object(Object* obj, string filename){
-  vector<Point> points = (*obj)->get_points();
+  vector<Point> points = obj->get_points();
   // Set up the JSON doc
   rapidjson::Document doc;
   rapidjson::Value json;
@@ -235,60 +232,214 @@ bool store_object(Object* obj, string filename){
   writer.StartObject();
   // Set up the object_name
   rapidjson::Value object_name;
-  object_name.SetString(req.name.c_str());
+  object_name.SetString(obj->get_name().c_str());
   json.AddMember("object_name", object_name, doc.GetAllocator());
   // Set up the object_type
   rapidjson::Value object_type;
-  object_type.SetString((*obj)->get_type().c_str());
+  object_type.SetString(obj->get_type().c_str());
   json.AddMember("object_type", object_type, doc.GetAllocator());
-  // Set up the object_rigidity
-  rapidjson::Value rigidity((*obj)->get_rigidity());
-  json.AddMember("object_rigidity", rigidity, doc.GetAllocator());
-  // Set up array of points
-  rapidjson::Value point_id_array;
-  point_id_array.SetArray();
-  vector<Point>::iterator iter;
-  for (iter = points.begin(); iter != points.end(); ++iter) {
-    rapidjson::Value id;
-    id.SetInt(iter->id);
-    point_id_array.PushBack(id, doc.GetAllocator());
+  if (obj->get_type() == "manual"){
+    Point my_center = obj->get_center();
+    rapidjson::Value center;
+    center.SetArray();
+    rapidjson::Value X;
+    X.SetDouble((double)my_center.x);
+    center.PushBack(X, doc.GetAllocator());
+    
+    rapidjson::Value Y;
+    X.SetDouble((double)my_center.y);
+    center.PushBack(Y, doc.GetAllocator());
+
+    rapidjson::Value Z;
+    Z.SetDouble((double)my_center.z);
+    center.PushBack(X, doc.GetAllocator());
+    json.AddMember("center", center, doc.GetAllocator());
+
+    vector<float> my_dim = obj->get_dimensions();
+    rapidjson::Value dim;
+    dim.SetArray();
+    rapidjson::Value x_scale;
+    x_scale.SetDouble((double)my_dim[0]);
+    rapidjson::Value y_scale;
+    x_scale.SetDouble((double)my_dim[1]);
+    rapidjson::Value z_scale;
+    z_scale.SetDouble((double)my_dim[2]);
+    dim.PushBack(x_scale, doc.GetAllocator());
+    dim.PushBack(y_scale, doc.GetAllocator());
+    dim.PushBack(z_scale, doc.GetAllocator());
+    json.AddMember("dimensions", dim, doc.GetAllocator());    
+
+    vector<float> my_angle = obj->get_rotation();
+    rapidjson::Value angle;
+    angle.SetArray();
+    rapidjson::Value w;
+    w.SetDouble((double)my_angle[0]);
+    rapidjson::Value x;
+    x.SetDouble((double)my_angle[1]);
+    rapidjson::Value y;
+    y.SetDouble((double)my_angle[2]);
+    rapidjson::Value z;
+    z.SetDouble((double)my_angle[3]);
+    angle.PushBack(w, doc.GetAllocator());
+    angle.PushBack(x, doc.GetAllocator());
+    angle.PushBack(y, doc.GetAllocator());
+    angle.PushBack(z, doc.GetAllocator());
+    json.AddMember("angle", angle, doc.GetAllocator());
+  } else {
+    // Set up the object_rigidity
+    rapidjson::Value rigidity(obj->get_rigidity());
+    json.AddMember("object_rigidity", rigidity, doc.GetAllocator());
+    // Set up array of points
+    rapidjson::Value point_id_array;
+    point_id_array.SetArray();
+    vector<Point>::iterator iter;
+    for (iter = points.begin(); iter != points.end(); ++iter) {
+      rapidjson::Value id;
+      id.SetInt(iter->id);
+      point_id_array.PushBack(id, doc.GetAllocator());
+    }
+    json.AddMember("object_points", point_id_array, doc.GetAllocator());
   }
-  json.AddMember("object_points", point_id_array, doc.GetAllocator());
   json.Accept(writer);
   writer.EndObject();
   fclose(fp);
   return true;
 }
-bool store_env(vector<Object*> objects, string env_name){
+bool store_env(string env_name){
   string folder_name;
   folder_name.append(ENV_EXT).append(env_name);
-  if(!mkdir(folder_name.c_str())) return false;
-  if(!mkdir(folder_name.append("objects/").c_str())) return false;
+  if(!mkdir(folder_name.c_str(), 777)) return false;
+  if(!mkdir(folder_name.append("objects/").c_str(), 777)) return false;
   vector<Object*>::iterator iter;
   vector<string> names;
-  for (int i = 0; i < object_vector_.size(); ++i) {
-    string filename = folder_name.append("objects/").append(iter->get_name());
-    store_object(*(*obj), filename);
-    names.push_back(obj->get_name());
+  for (iter = object_vector_.begin(); iter != object_vector_.end(); ++iter) {
+    string filename = folder_name.append("objects/").append((*iter)->get_name());
+    store_object(*iter, filename);
+    names.push_back((*iter)->get_name());
   }
-  
-  FILE *fp = fopen(folder_name.append("object_names").c_str(), "w");
+  rapidjson::Document doc;
+  rapidjson::Value json;
+  json.SetObject();
+  FILE *fp = fopen(folder_name.append("objects").c_str(), "w");
   rapidjson::FileStream fs(fp);
   rapidjson::PrettyWriter<rapidjson::FileStream> writer(fs);
   writer.StartObject();
   rapidjson::Value objects;
   objects.SetArray();
-  vector<string>::iterator iter;
-  for (iter = names.begin(); iter != names.end(); ++iter) {
+  vector<string>::iterator it;
+  for (it = names.begin(); it != names.end(); ++it) {
     rapidjson::Value name;
-    name = *iter;
+    name.SetString(it->c_str());
     objects.PushBack(name, doc.GetAllocator());
   }
   json.AddMember("objects", objects, doc.GetAllocator());
   json.Accept(writer);
   writer.EndObject();
   fclose(fp);
+  return true;
 }
+bool revive_object(string filename){
+  FILE *fp = fopen(filename.c_str(), "r");
+  rapidjson::FileStream fs(fp);
+  rapidjson::Document p;
+  if (p.ParseStream<0>(fs).HasParseError()) {
+    ROS_ERROR("Parse error on filestream from load_object(%s).", filename.c_str());
+    fclose(fp);
+    return false;
+  }
+  if (!p.IsObject()) {
+    ROS_ERROR("Couldn't load object from file: %s. Not a JSON object.", filename.c_str());
+    fclose(fp);
+    return false;
+  }
+  if (!p.HasMember("object_name")) {
+    ROS_ERROR("Object in (%s) has no name field.", filename.c_str());
+    fclose(fp);
+    return false;
+  }
+  if (!p.HasMember("object_type")) {
+    ROS_ERROR("Object in (%s) has no type associated with it.", filename.c_str());
+    fclose(fp);
+    return false;
+  }
+  string object_name = p["object_name"].GetString();
+  string t = p["type"].GetString();
+  if(t == "manual") {
+    ManualObject* obj = new ManualObject;
+    obj->init(object_count_, object_name);
+    rapidjson::Value &json_center = p["center"];
+    if (!json_center.IsArray() || json_center.Size() != 3) {
+      ROS_ERROR("Manual Object Center is not a properly sized array");
+      return false;
+    } else {
+      obj->SetCenter((float)json_center[(rapidjson::SizeType)0].GetDouble(), (float)json_center[1].GetDouble(), (float)json_center[2].GetDouble());
+    }
+    rapidjson::Value &dim = p["dimensions"];
+    if (!dim.IsArray() || dim.Size() != 3) {
+      ROS_ERROR("Manual Object dimensions is not a properly sized array");
+      return false; 
+    } else { 
+      obj->SetDim((float)dim[(rapidjson::SizeType)0].GetDouble(), (float)dim[1].GetDouble(), (float)dim[2].GetDouble());
+    }
+    rapidjson::Value &angle = p["angle"];
+    if (!angle.IsArray() || angle.Size() != 4) {
+      ROS_ERROR("Manual Object angle is not a properly sized array");
+      return false;
+    } else {
+      obj->SetAngle((float)angle[(rapidjson::SizeType)0].GetDouble(), (float)angle[1].GetDouble(), (float)angle[2].GetDouble(), (float)angle[3].GetDouble());
+    }
+    Object* casted = static_cast<Object*>(obj);
+    object_vector_.push_back(casted);
+    object_count_++;
+  } else {
+    bool rigid = p["rigid"].GetBool();
+    rapidjson::Value &points = p["points"];
+    vector<Point> object_points;
+    if (!points.IsArray()) {
+      ROS_ERROR("Object points in (%s) is not an array.", filename.c_str());
+      fclose(fp);
+      return false;
+    } else {
+      for(rapidjson::SizeType i = 0; i < points.Size(); ++i) {
+        Point point;
+        point.init();
+        point.id = points[i].GetInt();
+        if (std::find(ids_set_.begin(), ids_set_.end(), point.id) != ids_set_.end()) {
+        ROS_ERROR("Point id (%i) has already been set.  Load Object (%s) failed", point.id, filename.c_str());
+        fclose(fp);
+        return false;
+        }
+      }
+    }
+    PSObject* obj = new PSObject;
+    obj->init(object_count_, object_name, object_points, t, rigid);
+    object_count_++;
+    Object* casted = static_cast<Object*>(obj);
+    object_vector_.push_back(casted);
+  }
+  fclose(fp);
+  return true;
+}
+
+bool restore_env(string env_name) {
+  string env_file = ENV_EXT;
+  env_file.append(env_name);
+  FILE *fp = fopen(env_file.c_str(), "r");
+  rapidjson::FileStream fs(fp);
+  rapidjson::Document p;
+  if (!p.HasMember("objects")) {
+    ROS_ERROR("Environment file improperly stored");
+    return false;
+  } else {
+    rapidjson::Value &objects = p["objects"];
+    for(rapidjson::SizeType i = 0; i < objects.Size(); ++i){
+      string object_file = ENV_EXT;
+      object_file.append("objects/").append(objects[i].GetString());
+    }
+  }
+  return true; 
+}
+
 /////////////////////
 //// ROS SERVICES////
 //////////////////////////////////////////////////////////////////////////////// 
@@ -513,7 +664,17 @@ bool save_object(core_object_server::save_object::Request &req,
   string file = OBJECT_FILE_EXT;
   file += req.name;
   vector<Object*>::iterator obj = FindObjectByName(req.name, object_vector_);
-  return store_object(*(*obj), file);
+  return store_object(*obj, file);
+}
+
+bool save_env(core_object_server::save_env::Request &req,
+              core_object_server::save_env::Response &res) {
+  return store_env(req.name);
+}
+
+bool load_env(core_object_server::load_env::Request &req,
+              core_object_server::load_env::Response &res) {
+  return restore_env(req.name);
 }
 // load_object loads the object from the JSON file where the objects are stored
 // req: The ROS Service request for the core_object_server load_object service
@@ -672,6 +833,10 @@ int main(int argc, char **argv) {
       n.advertiseService("load_object", load_object);
   ros::ServiceServer man = 
       n.advertiseService("add_manual", add_manual);
+  ros::ServiceServer save_environment =
+      n.advertiseService("save_env", save_env);
+  ros::ServiceServer load_environment =
+      n.advertiseService("load_env", load_env);
   // The rate to loop throught the ROS calls
   // There is no need to do this at a higher rate than the PhaseSpace system
   ros::Rate loop_rate(frequency); 
